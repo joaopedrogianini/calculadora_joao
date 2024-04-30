967005b3cc922cfc82b04c4f21ee03e07f8104f0
joaopedrogianini <joaopedrogianinifranzin@gmail.com>
2024-04-30 08:29:34 -0300

    terminamos a calculadora

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"; 
+import {StyleSheet, View} from 'react-native'; 
+import Button from './src/components/Button'; 
+import Display from './src/components/Display'; 
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  state = {
+    displayValue: '0'
+  }
+
+  addDigit= n => {
+    this.setState = ({displayValue: n})
+  }
+  clearMemory => {
+    this.setState = ({displayValue: '0'})
+  }
+  setOperation = operation => {
+
+  }
+
+  return(
+    <View style={styles.container}> 
+    <Display value={this.state.displayValue} />
+    <View style={styles.button}>
+      <Button label='AC' triple onClick={this.clearMemory} />
+      <Button label='/' operation onClick={this.setOperation} />
+      <Button label='7' onClick={this.addDigit} />
+      <Button label='8' onClick={this.addDigit} />
+      <Button label='9' onClick={this.addDigit} />
+      <Button label='*' operation onClick={this.setOperation} /> 
+      <Button label='4' onClick={this.addDigit} />
+      <Button label='5' onClick={this.addDigit} />
+      <Button label='6' onClick={this.addDigit} />
+      <Button label='-' operation onClick={this.setOperation} /> 
+      <Button label='1' onClick={this.addDigit} />
+      <Button label='2' onClick={this.addDigit} />
+      <Button label='3' onClick={this.addDigit} />
+      <Button label='+' operation onClick={this.setOperation} /> 
+      <Button label='0' double onClick={this.addDigit} /> 
+      <Button label='.' onClick={this.addDigit} /> 
+      <Button label='=' operation onClick={this.addDigit} />
     </View>
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }, 
+  button: {
+    flexDirection: 'row', 
+    flexWrap: 'wrap',
+  }
+})
